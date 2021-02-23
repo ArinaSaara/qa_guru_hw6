e365778a32cbc6462bc6c0358e98c54277c7ec2d
Daryna <rinasaara@gmail.com>
2021-02-23 09:26:45 +0200

    Added .gitignore

--- src/test/java/com/github/SearchTest.java
+++ src/test/java/com/github/SearchTest.java
@@ -3,7 +3,6 @@ package com.github;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,6 +11,7 @@ public class SearchTest {
     String url = "https://www.google.com/",
             searchText = "Selenide",
             resultSearch = "selenide.org";
+
     @Test
     void searchSelenideWithGoogleTest() {
         open(url);
